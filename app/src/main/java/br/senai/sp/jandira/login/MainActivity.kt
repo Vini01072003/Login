c02eaad2dc0c4bc14fc595e350fa0b5160c4f166
Vinícius Alves <110607676+Vini01072003@users.noreply.github.com>
2023-03-07 11:20:13 -0300

    trabalho em andamento

---
+++ app/src/main/java/br/senai/sp/jandira/login/MainActivity.kt
@@ -0,0 +1,64 @@
+package br.senai.sp.jandira.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.login.ui.theme.LoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginTheme {
+                // A surface container using the 'background' color from the theme
+               
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginTheme {
+        Greeting("Android")
+    }
+    Column() {
+        Card(
+            modifier = Modifier
+                .fillMaxSize(),
+            shape = RoundedCornerShape(
+                topStart = 48.dp, topEnd = 48.dp
+            ),
+            backgroundColor = Color(
+                red = 79,
+                green = 54,
+                blue = 232
+            ),
+            //border = BorderStroke(2.dp, Color.Black)
+        )
+
+
